f61bf3ee40367abceecbd31ab0ffb91c70848f90
Borja Lorente <blorente.me@gmail.com>
2022-06-20 22:47:44 +0100

    Bounds are inaccurate, but we can solve that

--- src/layout.rs
+++ src/layout.rs
@@ -14,9 +14,25 @@ pub(crate) fn layout_mind_map(
 ) -> HashMap<MMItemId, LayoutPosition> {
     let mut positions: HashMap<MMItemId, LayoutPosition> = HashMap::new();
     let mut running_x = 0.0;
-    for (id, item) in map.nodes.iter() {
-        positions.insert(id.clone(), (running_x, 0.0));
-        running_x += item.bounds(&settings.render_item_settings).w + 10.0;
+    for root in map.roots().iter() {
+        let mut running_y = 0.0;
+        positions.insert(root.clone(), (running_x, running_y));
+        running_y += map
+            .item(&root)
+            .expect("failed to get handle to root")
+            .bounds(&settings.render_item_settings)
+            .h
+            + 10.0;
+
+        for child in map.children(root.clone()).iter() {
+            positions.insert(child.clone(), (running_x, running_y));
+        }
+        running_x += map
+            .item(&root)
+            .expect("Error retrieving the item")
+            .bounds(&settings.render_item_settings)
+            .w
+            + 10.0;
     }
     positions
 }
--- src/mind_map.rs
+++ src/mind_map.rs
@@ -1,4 +1,6 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, hash::Hash};
+
+use log::info;
 
 use crate::components::{MMItem, MMItemId};
 
@@ -6,15 +8,21 @@ pub(crate) struct MindMap {
     // Stores actual instances of nodes.
     // The items have to be boxed so that we can treat them as `dyn Renderizable` in layout.
     pub(crate) nodes: HashMap<MMItemId, Box<MMItem>>,
+
     // Links items to children
-    pub(crate) links: HashMap<MMItemId, Vec<MMItemId>>,
+    pub(crate) children: HashMap<MMItemId, Vec<MMItemId>>,
+
+    // A node may have exactly one parent.
+    // Root nodes have no parent.
+    pub(crate) parents: HashMap<MMItemId, Option<MMItemId>>,
 }
 
 impl MindMap {
     pub(crate) fn new() -> Self {
         MindMap {
             nodes: HashMap::new(),
-            links: HashMap::new(),
+            children: HashMap::new(),
+            parents: HashMap::new(),
         }
     }
 
@@ -25,13 +33,37 @@ impl MindMap {
     ) -> Result<MMItemId, String> {
         let (new_node, new_id) = MMItem::new(text);
         self.nodes.insert(new_id, Box::new(new_node));
+        self.parents.insert(new_id, maybe_parent);
         if let Some(parent) = maybe_parent {
-            if let Some(parent_entry) = self.links.get_mut(&parent) {
+            if let Some(parent_entry) = self.children.get_mut(&parent) {
                 parent_entry.push(new_id);
             } else {
-                self.links.insert(parent, vec![new_id]);
+                self.children.insert(parent, vec![new_id]);
             }
         }
         Ok(new_id)
+    }
+
+    pub(crate) fn roots(&self) -> Vec<MMItemId> {
+        self.parents
+            .iter()
+            .filter(|(_, parent)| parent.is_none())
+            .map(|(node, _)| node.clone())
+            .collect()
+    }
+
+    pub(crate) fn children(&self, of: MMItemId) -> Vec<MMItemId> {
+        info!("Getting children of {}: {:?}", of, self.children.get(&of));
+        if let Some(children) = self.children.get(&of) {
+            children.to_vec()
+        } else {
+            vec![]
+        }
+    }
+
+    pub(crate) fn item(&self, id: &MMItemId) -> Result<&Box<MMItem>, String> {
+        self.nodes
+            .get(id)
+            .ok_or("Error retrieving item".to_string())
     }
 }
